ee48d14d03b4c2d1cde488a323753d8e0ad7f9aa
alexmcdermid <alexander.mcdermid@gmail.com>
2022-10-13 23:30:52 -0700

    sol

---
+++ ruby/SubtractTheProductAndSumOfDigitsOfAnInteger.rb
@@ -0,0 +1,32 @@
+# Given an integer number n, return the difference between the product of its digits and the sum of its digits.
+ 
+# Example 1:
+
+# Input: n = 234
+# Output: 15 
+# Explanation: 
+# Product of digits = 2 * 3 * 4 = 24 
+# Sum of digits = 2 + 3 + 4 = 9 
+# Result = 24 - 9 = 15
+
+# Example 2:
+
+# Input: n = 4421
+# Output: 21
+# Explanation: 
+# Product of digits = 4 * 4 * 2 * 1 = 32 
+# Sum of digits = 4 + 4 + 2 + 1 = 11 
+# Result = 32 - 11 = 21
+
+# Constraints:
+
+# 1 <= n <= 10^5
+
+# @param {Integer} n
+# @return {Integer}
+def subtract_product_and_sum(n)
+  product = 1
+  sum = 0
+  n.to_s.split('').each{|num| product *= num.to_i and sum += num.to_i}
+  product - sum
+end
