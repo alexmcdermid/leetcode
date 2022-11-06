03491f207508a4c5ed005de3856e3f6b7fd76a07
alexmcdermid <alexander.mcdermid@alumni.ubc.ca>
2022-11-05 17:26:43 -0700

    formatting

--- ruby/SquaresOfASortedArray.rb
+++ ruby/SquaresOfASortedArray.rb
@@ -1,7 +1,5 @@
 # Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
 
- 
-
 # Example 1:
 
 # Input: nums = [-4,-1,0,3,10]
@@ -13,7 +11,6 @@
 # Input: nums = [-7,-3,2,3,11]
 # Output: [4,9,9,49,121]
  
-
 # Constraints:
 
 # 1 <= nums.length <= 104
